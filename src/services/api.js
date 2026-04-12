0b4808646a7fe7df49a8aa01c405ef61b05aa424
kumarsuraj345678 <kumarsuraj345678@gmail.com>
2026-04-13 02:07:38 +0530

    Fixed baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
-
 API.interceptors.request.use((req) => {
   const adminToken = localStorage.getItem("adminToken");
   const employeeToken = localStorage.getItem("employeeToken");
