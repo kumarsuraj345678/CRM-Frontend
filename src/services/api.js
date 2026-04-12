b25f22e6cf49858e0ba3a2a81374d8918f6b80ce
kumarsuraj345678 <kumarsuraj345678@gmail.com>
2026-04-13 02:12:05 +0530

    Revert API base URL to hardcoded value

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://crm-backend-1zni.onrender.com/api",
 });
 API.interceptors.request.use((req) => {
   const adminToken = localStorage.getItem("adminToken");
